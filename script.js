395c5240e934ce2d3b6eee8b685be3cde2f1dc3a
kikiriko200 <fujitakakikiriko200@yahoo.co.jp>
2019-03-31 18:19:37 +0900

    v0.2

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ $(function(){
   });
   var title = $(".blog-header h1").text();
   div.html('<div class="card-header"><p class="card-header-title has-text-centered has-text-white nem-b mt0 mr0 ml0 mb0">'+title+':目次(´▽｀)</p></dt></div>');
-  var countId = 1
+  var countId = 0;
   $(".blog-body h1,.blog-body h2,.blog-body h3,.blog-body h4,.blog-body h5,.blog-body h6").each(function(){
+    countId ++;
     var ttl = $(this).text();
     var lv = this.nodeName.toLowerCase();
     this.id = 'ttl-' + countId;
-    countId ++;
     div.append("<dd class='lv_"+lv+"'><a href='#'+this.id+'>"+ttl+"</a></dd>");
   });
   div.append("<div class='card-footer'><p class='card-footer-item'>&copy; DAFU</p></div>");
+  if(countId <= 1){//見出しが2個ない場合表示しない
+    div.css("display","none");
+  }
   if($(window).width()>769){
     $(".stick-side").prepend(div);
   }else{
